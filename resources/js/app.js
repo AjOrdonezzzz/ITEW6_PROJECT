327fee0b353579e291a59b97d8f2c00f8615eda7
Alrae Joseph Ordonez <ordonezalraejoseph85@gmail.com>
2026-03-15 11:59:38 +0800

    Initial Commit Laravel Set up W/ Vue

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+// Import components here if needed
+// app.component('example-component', ExampleComponent);
+
+app.mount('#app');
